4e1d6027de8b301ff152031591bd65ef03d38052
Ilham Abdullah <72017753+ilhamabdlh@users.noreply.github.com>
2022-08-11 16:29:53 +0700

    update cors fix

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"time"
 	"github.com/gin-gonic/gin"
 	"pln/jatim/pkg/controller"
 	"pln/jatim/pkg/db"
 	"github.com/spf13/viper"
-	"github.com/gin-contrib/cors"
 )
 
 func main() {
@@ -17,17 +15,7 @@ func main() {
 	dbUrl := viper.Get("DB_URL").(string)
 
 	r := gin.Default()
-	r.Use(cors.New(cors.Config{
-           AllowOrigins:     []string{"https://foo.com"},
-           AllowMethods:     []string{"PUT", "PATCH"},
-           AllowHeaders:     []string{"Origin"},
-           ExposeHeaders:    []string{"Content-Length"},
-           AllowCredentials: true,
-           AllowOriginFunc: func(origin string) bool {
-            return origin == "https://github.com"
-          },
-          MaxAge: 12 * time.Hour,
-        }))
+	r.Use(CORSMiddleware())
 
 	h := db.Init(dbUrl)
 
@@ -36,4 +24,21 @@ func main() {
 	// register more routes here
 
 	r.Run(port)
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, x-access-token")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	}
 }
