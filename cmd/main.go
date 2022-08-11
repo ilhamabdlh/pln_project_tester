d13c2b96ab1d965f1c85dfbeff7599a269526c9a
Ilham Abdullah <72017753+ilhamabdlh@users.noreply.github.com>
2022-08-11 16:33:12 +0700

    update cors

--- cmd/main.go
+++ cmd/main.go
@@ -28,7 +28,7 @@ func main() {
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:4001/login")
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, x-access-token")
